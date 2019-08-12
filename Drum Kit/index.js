83ef8553a3f9ddde5855547b7ac0036650a9d506
Sabrina <sabrina_veras@live.com>
2019-08-12 11:41:22 -0300

    drum kit challenge done

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,14 +1,62 @@
-function play(this){
-    console.log(this);
+document.addEventListener('keydown', function(event){
+
+    handleSounds(event.key);
+
+});
+
+
+function play(element){
+
+    handleSounds(element.id);
 
 }
 
+function handleSounds(position){
 
+    switch (position) {
+        case 'first', 'w':
 
-/*
-document.querySelector('button').addEventListener('click', handleClick);
+            var firstAudio = new Audio('sounds/tom-1.mp3')
+            firstAudio.play()
 
-function handleClick(){
-    alert('click');
+            break;
+        case 'second', 'a':
+
+            var secondAudio = new Audio('sounds/tom-2.mp3')
+            secondAudio.play()
+
+            break;
+        case 'third', 's':
+
+            var thirdAudio = new Audio('sounds/tom-3.mp3')
+            thirdAudio.play()
+
+            break;
+        case 'fourth', 'd':
+
+            var fourthAudio = new Audio('sounds/tom-4.mp3')
+            fourthAudio.play()
+
+            break;
+        case 'fiveth', 'j':
+
+            var fivethAudio = new Audio('sounds/crash.mp3')
+            fivethAudio.play()
+
+            break;
+        case 'sixth', 'k':
+
+            var sixthAudio = new Audio('sounds/kick-bass.mp3')
+            sixthAudio.play()
+
+            break;
+        case 'seventh', 'l':
+
+            var seventhAudio = new Audio('sounds/snare.mp3')
+            seventhAudio.play()
+
+            break;
+        default:
+            //console.log('nothing will happen');
+    }
 }
-*/
