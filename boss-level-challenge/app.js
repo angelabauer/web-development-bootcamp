093b96a499952339f8797a22136fc6aea17496e3
Sabrina <sabrina_veras@live.com>
2019-09-18 10:40:34 -0300

    challenge complete

--- boss-level-challenge/app.js
+++ boss-level-challenge/app.js
@@ -3,20 +3,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash")
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const posts = [];
+
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 
 // home page
 app.get('/', function(req, res){
 
-    res.render('home', { homeStartingContent: homeStartingContent});
+    res.render('home', { homeStartingContent: homeStartingContent, posts: posts});
 })
 
 
@@ -33,19 +39,46 @@ app.get('/contact', function(req, res){
     res.render('contact', { contactContent: contactContent});
 })
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+
+// compose page
+app.get('/compose', function(req, res){
+
+    res.render('compose');
+})
 
 
+// post :postTitle
+app.get('/posts/:postTitle', function(req, res){
+
+    let requestTitle = _.lowerCase(req.params.postTitle)
+
+    console.log('requestTitle', requestTitle);
+
+    posts.forEach(function(post){
+        if(_.lowerCase(post.postTitle) === requestTitle){
+
+            res.render('post', {post: post});
+        }
+    })
+
+});
 
 
+app.post('/compose', function(req, res){
 
+    let postTitle = req.body.postTitle
+    let postBody = req.body.postBody
 
+    let postObj = {
+        postTitle: postTitle,
+        postBody: postBody
+    }
 
+    posts.push(postObj)
 
+    res.redirect('/')
 
-
-
+})
 
 
 
